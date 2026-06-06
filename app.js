18453fcb26562ff061f3d8534fb6c0e69adb29bf
nandanchakraborty <nandancsebubt@gmail.com>
2026-06-06 15:39:32 +0600

    updated

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const weatherRoutes = require("./src/routes/weatherRoutes");
 const webhookRoutes = require("./src/routes/webhookRoutes");
 
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
